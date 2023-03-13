0a799df31a5f92f6bf7ade65e8f8c0f90d2c9506
Oleg Titov <hello.otitov@gmail.com>
2023-03-13 23:29:20 +0300

    use requestAnimationFrame

--- src/components/TimerItem.vue
+++ src/components/TimerItem.vue
@@ -6,37 +6,41 @@ const props = defineProps({
     type: Number,
     required: true,
     validator(value) {
-      return value > 10
+      return value >= 10
     }
   }
 })
 
 const responseTime = new Date(Date.now() + 1000 * props.seconds)
 const currentTimer = ref('')
-let intervalId = false
+let requestAnimationFrameId = false
+
+const startTimer = () => {
+  const t = responseTime - Date.now()
+  const miliSeconds = `000${Math.floor(t % 1000)}`.slice(-3)
+  const seconds = `00${Math.floor((t / 1000) % 60)}`.slice(-2)
+  const minutes = `00${Math.floor((t / 1000 / 60) % 60)}`.slice(-2)
+
+  currentTimer.value = `${minutes}:${seconds}:${miliSeconds}`
+
+  if (t <= 0) {
+    const finishHours = `00${responseTime.getHours()}`.slice(-2)
+    const finishMinutes = `00${responseTime.getMinutes()}`.slice(-2)
+    const finishSeconds = `00${responseTime.getSeconds()}`.slice(-2)
+    currentTimer.value = `00:00:000 - ${finishHours}:${finishMinutes}:${finishSeconds}`
+
+    return cancelAnimationFrame(requestAnimationFrameId)
+  }
+
+  requestAnimationFrame(startTimer)
+}
 
 onMounted(() => {
-  intervalId = setInterval(() => {
-    const t = responseTime - Date.now()
-    const miliSeconds = `000${Math.floor(t % 1000)}`.slice(-3)
-    const seconds = `00${Math.floor((t / 1000) % 60)}`.slice(-2)
-    const minutes = `00${Math.floor((t / 1000 / 60) % 60)}`.slice(-2)
-
-    currentTimer.value = `${minutes}:${seconds}:${miliSeconds}`
-
-    if (t <= 0) {
-      const finishHours = `00${responseTime.getHours()}`.slice(-2)
-      const finishMinutes = `00${responseTime.getMinutes()}`.slice(-2)
-      const finishSeconds = `00${responseTime.getSeconds()}`.slice(-2)
-      currentTimer.value = `00:00:000 - ${finishHours}:${finishMinutes}:${finishSeconds}`
-
-      clearInterval(intervalId)
-    }
-  }, 10)
+  requestAnimationFrameId = requestAnimationFrame(startTimer)
 })
 
 onUnmounted(() => {
-  clearInterval(intervalId)
+  cancelAnimationFrame(requestAnimationFrameId)
 })
 </script>
 
