01401874d413e804e1ae58c3c1b1dcf00636d487
Oleg Titov <hello.otitov@gmail.com>
2023-03-13 23:12:32 +0300

    add style and logic...

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,45 @@
 <script setup>
+import { ref } from "vue";
 import FormElement from './components/FormElement.vue'
 import TimerItem from './components/TimerItem.vue'
+
+const seconds = ref([]);
+
+const addSeconds = (item) => {
+  seconds.value.push(item);
+}
 </script>
 
 <template>
-  <FormElement />
-  <TimerItem :seconds="1" />
+  <main class="main">
+    <FormElement @add:seconds="addSeconds"/>
+
+    <ul class="main__timers">
+      <li v-for="(item, index) in seconds" :key="index">
+        #{{ index + 1 }}: <TimerItem :seconds="item" />
+      </li>
+    </ul>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style lang="scss" scoped>
+.main {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: rem(20px);
+  max-width: rem(1200px);
+  margin: 0 auto;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  @include mobile {
+    grid-template-columns: 1fr;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  &__timers {
+    padding: 0;
+    li {
+      display: flex;
+      gap: rem(10px);
+    }
   }
 }
 </style>
--- src/components/FormElement.vue
+++ src/components/FormElement.vue
@@ -6,21 +6,69 @@ const $emit = defineEmits(['add:seconds'])
 const seconds = ref(0)
 const minutes = ref(0)
 
-const isValidMinute = computed(() => minutes.value % 1 > 0)
-const isValidSeconds = computed(() => seconds.value % 10 > 0)
+const isValidMinute = computed(() => Number.isInteger(minutes.value) && minutes.value % 1 === 0)
+const isValidSeconds = computed(() => Number.isInteger(seconds.value) && seconds.value % 10 === 0)
 
 const addSeconds = () => {
   if (!isValidMinute.value || !isValidSeconds.value) return
-  return $emit('add:seconds', seconds.value)
+  $emit('add:seconds', seconds.value + minutes.value * 60)
+
+  seconds.value = 0;
+  minutes.value = 0;
 }
 </script>
 
 <template>
   <div class="form">
-    <label>Укажите минуты с шагом 1</label>
-    <input type="integer" v-model="minutes" :class="{ 'form__input--error': isValidMinute }" />
-    <label>Укажите секунды с шагом 10</label>
-    <input type="integer" v-model="seconds" :class="{ 'form__input--error': isValidSeconds }" />
-    <button @click="addSeconds">Добавить таймер</button>
+    <label class="form__label">Укажите минуты с шагом 1</label>
+    <input
+      type="number"
+      v-model="minutes"
+      class="form__input"
+      :class="{ 'form__input--error': !isValidMinute }"
+    />
+    <label class="form__label">Укажите секунды с шагом 10</label>
+    <input
+      type="number"
+      v-model="seconds"
+      class="form__input"
+      :class="{ 'form__input--error': !isValidSeconds }"
+    />
+    <button class="form__btn" @click="addSeconds">Добавить таймер</button>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: rem(10px);
+
+  &__input {
+    border: 1px solid gray;
+    padding: rem(5px 10px);
+    line-height: rem(35px);
+    outline: none;
+    border-radius: rem(10px);
+
+    &--error {
+      border-color: red;
+    }
+  }
+
+  &__btn {
+    border: 1px solid gray;
+    background: none;
+    padding: rem(5px 10px);
+    line-height: rem(35px);
+    border-radius: rem(10px);
+    cursor: pointer;
+    transition: color 0.5s, background-color 0.5s;
+
+    &:hover {
+      background: gray;
+      color: white;
+    }
+  }
+}
+</style>
